bf98d3c18ea707a63ce76c2ddaac5e325ccc910b
LALEY123 <128493509+LALEY123@users.noreply.github.com>
2023-03-21 09:01:59 -0500

    Add files via upload

---
+++ Index.php
@@ -0,0 +1,219 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>La Ley Computadoras</title>
+    <link rel="shortcut icon" href="Img/Logo/LOGO.png" type="image/x-icon">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css"
+        integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="css/Diseño.css">
+    <link rel="stylesheet" href="Java/Java.js">
+</head>
+
+<body>
+
+    <!--Header - Menu-->
+
+    <header>
+        <div class="container__header">
+            <div class="logo">
+                <a href="#">
+                    <img src="Img/Logo/LOGO-LALEY.png" alt="">
+                </a>
+            </div>
+
+            <div class="menu">
+                <nav>
+                    <ul>
+                        <li><a href="Index.php">Inicio</a></li>
+                        <li><a href="Tienda.php">Tienda</a></li>
+                        <li><a href="Contactos.php">Contactos</a></li>
+                    </ul>
+                </nav>
+
+                <a href="#" class="btn__quote">Cotizar</a>
+                <div class="main-header__container">
+                    <a href="IniciarSesion.php" class="main-header__link"><i class="fas fa-user"></i></a>
+                    <a href="CerrarSesion.php" class="main-header__exit"><i
+                            class="fa-solid fa-arrow-right-to-bracket"></i>
+
+                </div>
+
+                <div class="socialMedia">
+                    <a href="#">
+                        <img src="img/social media/facebook.png" alt="">
+                    </a>
+                    <a href="#">
+                        <img src="img/social media/instagram.png" alt="">
+                    </a>
+                    <a href="#">
+                        <img src="img/social media/twitter.png" alt="">
+                    </a>
+                    <a href="#">
+                        <img src="img/social media/youtube.png" alt="">
+                    </a>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <main>
+
+        <!--INICIO-->
+        <!--Portada de inicio-->
+        <div class="container__cover div__offset">
+
+            <div class="cover">
+
+                <div class="text__information-cover">
+                    <br>
+                    <br>
+                    <h1>Enciende tu mundo digital con nosotros</h1>
+                    <br>
+                    <p>No vendemos simples Computadoras, vendemos herramientas para tus sueños digitales a toda hora en
+                        la ciudad de Ambato</p>
+
+                    <div class="buttons__cover">
+                        <a href="IniciarSesion.php"><input type="button" class="btn__register-cover"
+                                value="Registrarse"></a>
+                        <input type="button" class="btn__readMore-cover" value="Leer más">
+                    </div>
+
+                </div>
+
+                <div class="media__cover">
+                    <video src="Img/About/animacion_cover.webm" autoplay playinline loop muted preload="auto"></video>
+                </div>
+
+
+            </div>
+        </div>
+
+        <!--Generador de confianza-->
+
+        <div class="container__trust container__card-primary">
+            <div class="trust card__primary">
+                <div class="text__trust text__card-primary">
+                    <p>PRODUCTOS DESTACADOS</p>
+                    <h1>Compra en linea con un solo toque</h1>
+                </div>
+                <br>
+                <br>
+                
+                <div class="container">
+
+                    <div class="card">
+                        <img src="img/About/ESCRITORIO.png">
+                        <h4>Laptop HP</h4>
+                        <p>- 10 dolares
+                            <br>
+                            -10dolares
+                        </p>
+                       
+                        <span class="hide" id="hidetext">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum dolorum, dignissimos repellpisci?</p>
+                        </span>
+                        
+                        <a href="" id="hidetext_leer">Leer más</a>
+                    </div>
+
+                    <div class="card">
+                        <img src="img/About/WEB.jpg">
+                        <h4>Teclado Gamer</h4>
+                        <p></p>
+                        <a href="#">Leer más</a>
+                    </div>
+
+                    <div class="card">
+                        <img src="img/About/COMPU.jpg">
+                        <h4>iPad</h4>
+                        <p></p>
+                        <a href="#">Leer más</a>
+                    </div>
+
+                </div>
+                <br><br><br><br>
+                <div class="trust card__primary">
+                    <div class="text__trust text__card-primary">
+                        <h1>Lemas de la Empresa</h1>
+                         <p>Conectando Tu mundo Potenciando tu futuro</p>
+                    </div>
+                <div class="container__trust container__box-cardPrimary">
+                    <div class="card__trust box__card-primary">
+                        <img src="img/Trust area/anchor.png" alt="">
+                        <h2>Servicio empresarial completo</h2>
+                        <p>Deja que nos encarguemos de todo para que puedas transformar tu negocio con soluciones tecnologicas </p>
+                    </div>
+                    <div class="card__trust box__card-primary">
+                        <img src="img/Trust area/archive.png" alt="">
+                        <h2>Resivos y comprobantes al dia </h2>
+                        <p>Facilitando tus transacciones, un comprobante a la vez</p>
+                    </div>
+                    <div class="card__trust box__card-primary">
+                        <img src="img/Trust area/user.png" alt="">
+                        <h2>Satisfacción del usuario garantizada</h2>
+                        <p>Tu satisfaccion es nuestra prioridad, Garantizado.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!--Sobre nosotros - Nuestro equipo-->
+
+        <div class="container__about div__offset">
+            <div class="about">
+                <div class="text__about">
+                    <h1>El equipo más innovador</h1>
+                    <p>
+                        En nuestra empresa, estamos comprometidos en ofrecer a nuestros clientes las mejores soluciones tecnologicas del mercado.Para lograr esto, contamos con un equipo altamente capacitado y con un enfoque en la innovacion constante.Nuestas ventas estan respaldadas por la ultima tecnologia disponible, lo que nos permite ofrecer a nuestros clientes los produtos mas avanzados y de la mas alta calidad. 
+                    </p>
+                
+                </div>
+
+                <div class="image__about">
+                    <img src="img/About/about-1.png" alt="">
+                    <img src="img/About/about-2.png" alt="">
+                </div>
+            </div>
+
+        </div>
+        <br><br>
+
+    </main>
+    <!--footer-->
+    <footer class="main-footer">
+        <div class="footer__section">
+            <h2 class="footer__title">Acerca de nosotros</h2>
+            <br>
+            <p class="footer__txt">Somos una empresa lider en el mercado de la tecnologia en la ciudad de ambato ofreciendo soluciones integrales y productos de alta calidad a nuestros clientes desde hace muchos años</p>
+        </div>
+        <div class="footer__section">
+            <h2 class="footer__title">Ubicacion </h2>
+            <br>
+            <p class="footer__txt">Estamos ubicados en la ciudad de Ambato Av. 12 de Noviembre y Ayllon</p>
+        </div>
+        <div class="footer__section">
+            <h2 class="footer__title">Links de pagina</h2>
+            <br>
+            <a href="" class="footer__link">Home</a>
+            <a href="" class="footer__link">Tienda</a>
+            <a href="" class="footer__link">Contactos</a>
+
+        </div>
+        <div class="footer__section">
+            <h2 class="footer__title">Iniciar Sesion</h2>
+            <br>
+            <p class="footer__txt">Al suscribirse a nuestra lista de correo, siempre recibirá nuestras últimas noticias y actualizaciones.
+            </p>
+
+        </div>
+
+    </footer>
+
+</body>
+
+</html>
